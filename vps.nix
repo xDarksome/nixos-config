06048d90a83b8d81ebfad6b01fb9ae18eaf86480
Darksome <dev@darkso.me>
2023-12-17 19:56:46 +0000

    vps: specify allowed ssh key

--- vps.nix
+++ vps.nix
@@ -10,4 +10,8 @@
       KbdInteractiveAuthentication = false;
     };
   };
+
+  users.users.${username}.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJBJcLJ7ZGkut93mp+aAdn3NQVmV3oWIE4xQLcZo3mkl dev@darkso.me"
+  ];
 }
