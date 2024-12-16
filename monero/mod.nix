0e1df4cfcca70cf6799121bbbc82876981cdb5b1
Darksome <dev@darkso.me>
2024-12-16 21:45:42 +0000

    merge upstream

--- monero/mod.nix
+++ monero/mod.nix
@@ -6,6 +6,17 @@
 }: {
   services.monero = {
     enable = true;
+    extraConfig = ''
+      no-igd=1
+    '';
+  };
+
+  services.tor = {
+    enable = true;
+    settings = {
+      HiddenServiceDir = "/var/lib/tor/monero-service/";
+      HiddenServicePort = "18081 127.0.0.1:18081";
+    };
   };
 
   environment.systemPackages = with pkgs; [monero-gui];
