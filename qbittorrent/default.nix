6be0c410beda0e23e9c7e3f05cdc0af6b5c9b2a6
Darksome <dev@darkso.me>
2025-06-06 22:06:50 +0000

    qbittorrent: symlink config

--- qbittorrent/default.nix
+++ qbittorrent/default.nix
@@ -1,4 +1,5 @@
 {
+  lib,
   pkgs,
   username,
   home-manager,
@@ -16,32 +17,8 @@
     }))
   ];
 
-  home-manager.users.${username}.home.file = {
-    ".config/qBittorrent/iceberg.qbtheme".source = ./iceberg.qbtheme;
-    ".config/qBittorrent/qBittorrent.conf" = {
-      force = true;
-      text = ''
-        [AddNewTorrentDialog]
-        DownloadPathHistory=
-        Enabled=true
-        RememberLastSavePath=false
-        SavePathHistory=/home/${username}/Downloads
-
-        [GUI]
-        StartUpWindowState=Minimized
-
-        [LegalNotice]
-        Accepted=true
-
-        [Preferences]
-        General\ExitConfirm=false
-        General\UseCustomUITheme=true
-        General\CustomUIThemePath=/home/${username}/.config/qBittorrent/iceberg.qbtheme
-
-        [TorrentProperties]
-        CurrentTab=4
-        Visible=true
-      '';
-    };
-  };
+  system.activationScripts.symlinks.text = lib.mkAfter ''
+    rm -rf /home/${username}/.config/qBittorrent
+    ln -s /home/${username}/nixos-config/qbittorrent/config /home/${username}/.config/qBittorrent
+  '';
 }
